3832af70b059782bc7e2187733e5707f1f2a7190
buziusbastus1 <kubaotreba121@wp.pl>
2023-02-19 19:57:36 +0100

    orbit

--- src/Model.jsx
+++ src/Model.jsx
@@ -2,6 +2,7 @@ import { Stage, OrbitControls } from '@react-three/drei'
 import { Canvas, extend } from '@react-three/fiber'
 import React from 'react'
 import Model from './assets/guitar'
+import './Model.scss'
 extend({ OrbitControls })
 
 // function Model (props) {
@@ -10,12 +11,12 @@ extend({ OrbitControls })
 // }
 function Render () {
   return (
-  <Canvas shadows dpr={[1, 2]} camera={{ fov: 45 }} style={{ position: 'absolute'/*, width: '600px', height: '600px' */ }} >
+  <Canvas className="my-canvas" shadows dpr={[1, 2]} camera={{ fov: 45 }} style={{ position: 'absolute'/*, width: '600px', height: '600px' */ }} >
       <ambientLight />
 <color attach="background" args={['#9EABA4']}/>
 <OrbitControls speed={1.5} global zoom={0.5} polar={[-0.1, Math.PI / 4]}/>
   <Stage environment={null} intensity={1} contactShadowOpacity={0.1} shadowBias={-0.0015}>
-    <Model scale={0.11}></Model>
+    <Model scale={0.11} style={{ pointerEvents: 'none' }}></Model>
   </Stage>
   </Canvas>
   )
